2c0c082ef85419f51efc38d82d732bef074c6cc6
shinealexander <>
2021-06-08 22:12:58 +0900

    달력 만드는중..

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -19,11 +19,6 @@ const Week = styled.article`
 `;
 const CalendarDays = () => {
   const week = ["일", "월", "화", "수", "목", "금", "토"];
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth();
-  const firstDay = new Date(year, month, 1);
-  const lastDay = new Date(year, month + 1, 0);
 
   return (
     <>
@@ -38,21 +33,76 @@ const CalendarDays = () => {
   );
 };
 
+const CalendarDates = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+
+  /*
+  [
+    [30, 31, 1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24, 25, 26],
+    [27, 28, 29, 30, 1, 2, 3]
+  ]
+  */ 
+  let days = [];
+
+  // 이전달 full date
+  const prevMonthLastFullDate = new Date(year, month, 0);
+  // 이번달 full date
+  const currentMonthLastFullDate = new Date(year, month + 1, 0);
+
+  // 이전달 마지막 날
+  const prevLastDate = prevMonthLastFullDate.getDate();
+  // 이전달 마지막 요일
+  const prevLastDay = prevMonthLastFullDate.getDay();
+
+  // 이전달 마지막 날 - 이전달 마지막 요일
+  const prevDays = prevLastDate - prevLastDay;
+
+  for(let i = prevDays; i <= prevLastDate; i++) {
+    days.push(i);
+  }
+
+  // 이번달 마지막 날
+  const currentDate = currentMonthLastFullDate.getDate();
+  // 이번달 마지막 요일
+  const currentDay = currentMonthLastFullDate.getDay();
+
+  for(let j = 1; j <= currentDate; j++) {
+    days.push(j);
+  }
+
+  // 이번달 weeks
+  const currentWeeks = Math.ceil(days.length / 7);
+
+  const create2DepthDate = days.reduce((acc, cur, index) => {
+    const multiple = ((index ) / 7);
+    const arrayIndex = Math.floor(multiple);
+    acc[arrayIndex][index] = cur;
+    return acc;
+  }, Array.from(Array(currentWeeks), () => Array(7)));
+
+  console.log('create2DepthDate = ',create2DepthDate);
+
+  // 다음달
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  
+
+  return (
+    <article aria-label="날짜">
+      
+    </article>
+  );
+}
+
 const Calendar = () => {
   const year = new Date().getFullYear();
-  const month = new Date().getMonth();
+  const month = new Date().getMonth() + 1;
 
-  // 
-  const prevMonthLastDay = new Date(year, month, 0);
-  const currentMonthLastDay = new Date(year, month + 1, 0);
-
-  const prevDate = prevMonthLastDay.getDate();
-  const prevDay = prevMonthLastDay.getDay();
-
-  const currentDate = currentMonthLastDay.getDate();
-  const currentDay = currentMonthLastDay.getDay();
-
-  console.log(prevDate, prevDay, currentDate, currentDay);
   return (
     <main>
       <Header>
@@ -63,7 +113,7 @@ const Calendar = () => {
         <button>&gt;</button>
       </Header>
       <CalendarDays />
-      <article aria-label="날짜"></article>
+      <CalendarDates />
     </main>
   );
 };
